35af0e27694056a3164fbc7c1efc220da0ef506e
AGbaby09 <agbotsem@gmail.com>
2023-10-05 19:52:41 +0000

    commit

--- script.js
+++ script.js
@@ -54,8 +54,8 @@ function getMonth(m){
     return months[m];
 }
 
-async function fetchWeatherData(){
-    await fetch(`https://api.weatherapi.com/v1/current.json?key=144eda60e2ec4b29abe10238230510&q=${cityInput}&aqi=no`)
+function fetchWeatherData(){
+    fetch(`https://api.weatherapi.com/v1/current.json?key=144eda60e2ec4b29abe10238230510&q=${cityInput}&aqi=no`)
     .then(response => response.json())
     .then(data => {
         // console.log(data);
